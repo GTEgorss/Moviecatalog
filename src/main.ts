2946346b2323145d0ef99d02d60d3ae8845e8e0e
GTEgorss <noartu@gmail.com>
2023-03-31 19:10:53 +0300

    feat: i got scammed by express-handlebars and by hbs as well

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const express = require('express');
-  const hbs = require('hbs');
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
