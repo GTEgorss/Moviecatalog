f8bf3426da58c08b913dfd2e2873e92875c9fd63
GTEgorss <noartu@gmail.com>
2023-05-09 17:59:05 +0300

    fix: path

--- public/JS/Global.js
+++ public/JS/Global.js
@@ -1,8 +1,8 @@
-const global_url = 'http://localhost:2002';
-// const global_url = 'https://gtegorss-moviecatalog.onrender.com';
+// const global_url = 'http://localhost:2002';
+const global_url = 'https://gtegorss-moviecatalog.onrender.com';
 
-const apiDomain = 'http://localhost:2002';
-// const apiDomain = 'https://gtegorss-moviecatalog.onrender.com';
+// const apiDomain = 'http://localhost:2002';
+const apiDomain = 'https://gtegorss-moviecatalog.onrender.com';
 
 const username = getCookie();
 
