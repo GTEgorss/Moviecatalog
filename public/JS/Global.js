80ef5fc39a363d03188cc6835545220d4669b16e
GTEgorss <noartu@gmail.com>
2023-05-09 18:18:43 +0300

    fix: chrome cookie dumb system LOL

--- public/JS/Global.js
+++ public/JS/Global.js
@@ -1,20 +1,19 @@
-// const global_url = 'http://localhost:2002';
-const global_url = 'https://gtegorss-moviecatalog.onrender.com';
+const global_url = 'http://localhost:2002';
+// const global_url = 'https://gtegorss-moviecatalog.onrender.com';
 
-// const apiDomain = 'http://localhost:2002';
-const apiDomain = 'https://gtegorss-moviecatalog.onrender.com';
+const apiDomain = 'http://localhost:2002';
+// const apiDomain = 'https://gtegorss-moviecatalog.onrender.com';
 
 const username = getCookie();
 
 function getCookie() {
   const name = 'username=';
   let cookies = decodeURIComponent(document.cookie);
-  console.log(cookies);
   const cookiesArray = cookies.split(';');
   let res;
   cookiesArray.forEach((val) => {
     if (val.trim().indexOf(name) === 0) {
-      res = val.substring(name.length);
+      res = val.trim().substring(name.length);
     }
   });
 
